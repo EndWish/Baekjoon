83d5b59656d79c698d96183ea1da6607e7319350
Kim SI In <90082921+EndWish@users.noreply.github.com>
2022-06-10 12:06:38 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -1,29 +1,9 @@
 #include <iostream>
+#include <bit>
 
-using namespace std;
-
-int x;
-
-int CountOneBit(int num);
-
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
-	//최기화,입력값 받기
-	cin >> x;
-
-	//문제해결
-
-	//출력
-	cout << CountOneBit(x);
-
-}
-
-int CountOneBit(int num) {
-	int cnt = 0;
-	while (num) {
-		num &= (num - 1);
-		++cnt;
-	}
-	return cnt;
+int main() 
+{ 
+	unsigned int x; 
+	std::cin >> x; 
+	std::cout << std::popcount(x);
 }
