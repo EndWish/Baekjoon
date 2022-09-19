8126344042e7c013658271d2734e4fe8162c002c
Kim SI In <90082921+EndWish@users.noreply.github.com>
2022-09-19 23:55:55 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <queue>
+#include <tuple>
+#include <ranges>
+#include <algorithm>
+#include <numeric>
+//#include <format>
+
+using namespace std;
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	//초기화,입력값 받기
+	cout << 566 << "\nkyh298494";
+
+	//문제해결
+
+	//출력
+
+}
