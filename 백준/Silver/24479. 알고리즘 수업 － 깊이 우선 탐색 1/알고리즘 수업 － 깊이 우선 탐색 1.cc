63c02710060a8148e9e4cc20ac947ed7f9f4fc20
Kim SI In <90082921+EndWish@users.noreply.github.com>
2023-02-01 20:35:02 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 280 ms, Memory: 26612 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <set>
+#include <algorithm>
+#include <ranges>
+#include <numeric>
+
+using namespace std;
+
+int n, m, r;
+array<set<int>, 100'001> edges;
+array<bool, 100'001> visit;
+array<int, 100'001> ans;
+
+int cnt;
+
+void DFS(int node);
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	//초기화,입력값 받기
+	cin >> n >> m >> r;
+	for (int i = 0; i < m; ++i) {
+		int u, v;
+		cin >> u >> v;
+		edges[u].insert(v);
+		edges[v].insert(u);
+	}
+
+	//문제해결
+	DFS(r);
+	for (int i = 1; i <= n; ++i) {
+		cout << ans[i] << "\n";
+	}
+	
+}
+
+void DFS(int node) {
+	ans[node] = ++cnt;
+	visit[node] = true;
+	for (int nextNode : edges[node]) {
+		if (!visit[nextNode])
+			DFS(nextNode);
+	}
+}
