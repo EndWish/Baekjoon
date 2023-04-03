39b45f8c613d3c24970feb21e2cbefe6baf27630
Kim SI In <90082921+EndWish@users.noreply.github.com>
2023-04-03 17:16:25 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.cc
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.cc
@@ -14,7 +14,7 @@ int n, m, l, k;
 vector<int> xList, yList;
 vector<pii> points;
 
-int GetShootingStar(int x, int y);
+int GetShootingStar(int x, int y, bool sorted);
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
@@ -37,21 +37,34 @@ int main() {
 	yList.erase(ranges::unique(yList).begin(), yList.end());
 
 	// 정답 찾기
+	ranges::sort(points);
 	int ans = numeric_limits<int>::max();
 	for (int x : xList) {
 		for (int y : yList) {
-			ans = min(ans, GetShootingStar(x, y));
+			ans = min(ans, GetShootingStar(x, y, true));
 		}
 	}
 	cout << ans;
 	
 }
 
-int GetShootingStar(int x, int y) {
+int GetShootingStar(int x, int y, bool sorted) {
 	int result = points.size();
-	for (auto [px, py] : points) {
-		if (x <= px && px <= x + l && y <= py && py <= y + l)
-			--result;
+	if (sorted) {
+		int dropNum = ranges::upper_bound(points, x - 1, {}, &pii::first) - points.begin();
+		for (auto [px, py] : points | views::drop(dropNum))
+			if (x <= px && px <= x + l) {
+				if (y <= py && py <= y + l)
+					--result;
+			}
+			else {
+				break;
+			}	
+	}
+	else {
+		for (auto [px, py] : points)
+			if (x <= px && px <= x + l && y <= py && py <= y + l)
+				--result;
 	}
 	return result;
 }
