5a22fc44bfc53d3bcfc2be1aa187d9feaa944b5e
Kim SI In <90082921+EndWish@users.noreply.github.com>
2023-04-17 18:08:25 +0900

    [Gold V] Title: 문자열 복사, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2195. 문자열 복사/문자열 복사.cc
+++ 백준/Gold/2195. 문자열 복사/문자열 복사.cc
@@ -8,51 +8,26 @@
 
 using namespace std;
 
-class Node {
-public:
-	unordered_map<char, Node*> next;
 
-	void Insert(const string str, int startIndex) {
-		if (str.size() <= startIndex)
-			return;
-
-		char ch = str[startIndex];
-		auto it = next.find(ch);
-		if (it != next.end()) {
-			it->second->Insert(str, startIndex + 1);
-		}
-		else {
-			next[ch] = new Node();
-			next[ch]->Insert(str, startIndex + 1);
-		}
-	}
-};
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-	Node root;
+	string s, p;
+	cin >> s >> p;
 
-	string S, P;
-	cin >> S;
-	for (int i = 0; i < S.size(); ++i) {
-		root.Insert(S, i);
-	}
-
-	cin >> P;
-	int ans = 1;
-	Node* pNode = &root;
-	for (int i = 0; i < P.size(); ++i) {
-		char ch = P[i];
-		if (pNode->next.contains(ch)) {
-			pNode = pNode->next[ch];
+	int result = 0;
+	for (int i = 0; i < p.size();) {
+		int maxLen = 0;
+		for (int j = 0; j < s.size(); ++j) {
+			int len = 0;
+			while (s[j + len] == p[i + len])
+				++len;
+			maxLen = max(maxLen, len);
 		}
-		else {
-			pNode = root.next[ch];
-			++ans;
-		}
+		i += maxLen;
+		++result;
 	}
-
-	cout << ans;
+	cout << result;
 }
 
